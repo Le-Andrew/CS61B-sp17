f481a5abf93a8bc28fbd64ea25c8ac9aeb2b70d1
Andrew Le <andrewle2501@gmail.com>
2017-12-27 17:01:43 +1000

    Added resizing for adding

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -9,21 +9,57 @@ public class ArrayDeque<Item> {
         items = (Item[]) new Object[maxSize];
     }
 
-    private int getModdedIndex() {
-        return (first < 0) ? (first + maxSize) : first;
+    private int getCircularIndex(int index) {
+        return (index < 0) ? (index + maxSize) : index;
     }
 
     private int getLastIndex() {
-        return ( (first + currentSize) % maxSize ) - 1;
+        return ( (first + currentSize - 1) % maxSize );
+    }
+
+    private boolean isFull() {
+        return currentSize == maxSize;
+    }
+
+    // TODO: Rename to extends array
+    private void resizeArray(int newSize) {
+        Item[] newItems = (Item[]) new Object[newSize];
+
+        /* Start placing items from the centre */
+        int centreOfQueue = newSize / 2;
+        int j = centreOfQueue - (currentSize / 2);
+        int newFirst = j;
+        for (int i = 0; i < currentSize; i++) {
+            int index = getCircularIndex(first + i);
+            newItems[j] = items[index];
+            j++;
+        }
+
+        /* Update pointers */
+        first = newFirst;
+        items = newItems;
+        maxSize = newSize;
     }
 
     public void addFirst(Item item) {
+
+        /* Need to re-size array and then re-order if necessary */
+        if (isFull()) {
+            resizeArray(maxSize * 2);
+        }
+
         first--;
         currentSize++;
-        items[getModdedIndex()] = item;
+        items[getCircularIndex(first)] = item;
+
+
     }
 
     public void addLast(Item item) {
+        if (isFull()) {
+            resizeArray(maxSize * 2);
+        }
+
         currentSize++;
         items[getLastIndex()] = item;
     }
@@ -50,11 +86,19 @@ public class ArrayDeque<Item> {
 
     public static void main(String[] args) {
         ArrayDeque<Integer>  test = new ArrayDeque<Integer>();
-        test.addFirst(5);
-        test.addFirst(10);
+        test.addFirst(1);
+        test.addFirst(2);
         test.addFirst(3);
+        test.addFirst(4);
         test.addFirst(5);
         test.addFirst(6);
+        test.addFirst(7);
+        test.addFirst(8);
+        test.addFirst(9);
+        test.addLast(2);
+        test.addLast(3);
+        test.addLast(4);
+        test.addLast(5);
     }
 
 
